ca845779c8acb3e95365a7193c112ceb6c14209a
unknown <kanahayameiliea@gmail.com>
2023-10-20 08:47:28 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style type="text/css">
+        body {
+            background-image: url(asset/kertas.jpeg);
+            background-size: cover;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <div class="box1">
+            <img src="asset/perp.png" width="230" height="200">
+            <br>
+            <h3 style="color: yellow;">
+                <b>
+                    HALAMAN LOGIN
+                </b>
+            </h3>
+            <br>
+            <table border="0" align="center">
+                <tr>
+                    <td style="color:white;">Username</td>
+                    <td>
+                        <input type="text" placeholder="Username..." />
+                    </td>
+                </tr>
+                <tr>
+                    <td style="color:white;">Password</td>
+                    <td>
+                        <input type="password" placeholder="Password..." />
+                    </td>
+                </tr>
+                <tr>
+                    <th colspan="2">
+                        <center><a href="admin.php"><input type="button" value="Login" name="login" /></a></center>
+                    </th>
+                </tr>
+            </table>
+            <p style="color:yellow;">
+        Copyright &copy; 2024 | PERPUSTAKAAN SMKN 9 SEMARANG
+    </p>
+        </div>
+    </center>
+</body>
+
+</html>
